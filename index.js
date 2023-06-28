10bed78c72df4882bea51ad6904e5d4bd478b534
Itsannm <ann.mwanzia@student.moringaschool.com>
2023-06-28 19:50:09 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Your code here
+// Function to move the DODGER to the left
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace('px', '');
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 4}px`;
+    }
+  }
+  
+  // Function to move the DODGER to the right
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace('px', '');
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left < 360) {
+      dodger.style.left = `${left + 4}px`;
+    }
+  }
+  
+  // Export the functions
+  module.exports = {
+    moveDodgerLeft,
+    moveDodgerRight
+  };
+  
